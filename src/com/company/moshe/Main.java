659ae4b9f1d9b47ddcfe06b2e340fe3ea01c6e73
lionsgrandson <43815731+lionsgrandson@users.noreply.github.com>
2020-04-10 15:41:12 +0300

    added settings

--- src/com/company/moshe/Main.java
+++ src/com/company/moshe/Main.java
@@ -12,16 +12,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
 
-import static java.awt.GridBagConstraints.CENTER;
-import static java.awt.GridBagConstraints.SOUTH;
-
 public class Main {
     int[] rndNUmbers = new int[3];
     int numbers1 = 0;
     int numbers2 = 0;
     int signNum = 0;
-    int score=0;
-
+    int score = 0;
+    int settingNum = 0;
+    int settingSign = 0;
 
     public static void main(String[] args) {
         Main main = new Main();
@@ -33,6 +31,7 @@ public class Main {
     }
 
     public void mainRun() throws IOException {
+
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException e) {
@@ -50,28 +49,33 @@ public class Main {
         JLabel questionLabel = new JLabel();
         JLabel answerHereLabel = new JLabel("Answer Here");
         JLabel draftLabel = new JLabel("Draft");
-        JTextField answerText = new JTextField("" , 10);
+        JTextField answerText = new JTextField("", 10);
         JTextArea thinkText = new JTextArea(10, 10);
         JButton button = new JButton();
+        JButton seting = new JButton("Set");
+
+        seting.setBounds(0, 210, 50, 50);
+        seting.setVisible(false);
 
         questionLabel.setText("");
-        questionLabel.setBounds(25,10,200,40);
+        questionLabel.setBounds(25, 10, 200, 40);
 
-        answerHereLabel.setBounds(10,60,100,20);
+        answerHereLabel.setBounds(10, 60, 100, 20);
 
         Border thickBorder = new LineBorder(Color.BLACK, 2);
         answerText.setBorder(thickBorder);
-        answerText.setBounds(10,80,140,40);
+        answerText.setBounds(10, 80, 140, 40);
 
         button.setText("Enter");
-        button.setBounds(10,120,100,40);
+        button.setBounds(10, 120, 100, 40);
 
-        draftLabel.setBounds(200,10,50,30);
+        draftLabel.setBounds(200, 10, 50, 30);
 
-        thinkText.setBounds(155,40,120,200);
+        thinkText.setBounds(155, 40, 120, 200);
         thinkText.setBorder(thickBorder);
         thinkText.setLineWrap(true);
 
+        frame.add(seting);
         frame.add(questionLabel);
         frame.add(answerHereLabel);
         frame.add(answerText);
@@ -88,43 +92,109 @@ public class Main {
         String ans = "";
         String question = "";
 
-        String filename = "score.txt";
+        String scoreFileName = "score.txt";
+        String settingFileName = "setting.txt";
 
         String userDirectory = System.getProperty("user.name");
 
-        String absoluteFilePath =  "C:\\Users\\" + userDirectory + "\\Documents\\" + filename;
+        String absoluteFilePath = "C:\\Users\\" + userDirectory + "\\Documents\\" + scoreFileName;
+        String settingFilePath = "C:\\Users\\" + userDirectory + "\\Documents\\" + settingFileName;
+
 
         //absoluteFilePath = workingDirectory + System.getProperty("file.separator") + filename;
 //        System.out.println(absoluteFilePath);
-        File file = new File(absoluteFilePath);
-        Path path = Paths.get(file.getPath());
-        if (!file.exists()) {
-            file.createNewFile();
+        File scoreFile = new File(absoluteFilePath);
+        Path scorePath = Paths.get(scoreFile.getPath());
+
+        File settingFiles = new File(settingFilePath);
+        Path settingPath = Paths.get(settingFiles.getPath());
+        if (!settingFiles.exists()) {
+            settingFiles.createNewFile();
+        }
+        if (!scoreFile.exists()) {
+            scoreFile.createNewFile();
         }
 
-        BufferedReader br = Files.newBufferedReader(path);
+        BufferedReader br = Files.newBufferedReader(scorePath);
+        BufferedReader brSet = Files.newBufferedReader(settingPath);
+
         if (br.readLine() != null) {
             score = Integer.parseInt(br.readLine());
         } else {
             score = 0;
         }
+        if (brSet.readLine() != null) {
+            settingNum = Integer.parseInt(brSet.readLine());
+            if (Integer.parseInt(brSet.readLine()) < 5 && brSet.readLine() != null) {
+                settingSign = Integer.parseInt(brSet.readLine());
+            } else {
+                settingSign = 3;
+            }
+        } else {
+            score = 11;
+            settingSign = 3;
+        }
+
         //it will delete the file context every time, but only after it saves the score to the new program run,
         // it will then save it again when you're done.
-        BufferedWriter bw = new BufferedWriter(new FileWriter(path.toString(), false));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(scorePath.toString(), false));
+        BufferedWriter bwSet = new BufferedWriter(new FileWriter(settingPath.toString(), false));
         newQ(questionLabel);
 
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                saveScore(bw,br,questionLabel,answerText,score);
+                saveScore(bw, br, questionLabel, answerText, score, settingNum, bwSet, brSet, settingSign);
             }
         });
+        seting.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame settingJ = new JFrame("settings");
+                JPanel setPan = new JPanel();
+                JButton setBTN = new JButton("Submit");
+                JTextField setTXT = new JTextField(10);
+
+                setTXT.setBounds(10, 25, 100, 50);
+
+                setBTN.setBounds(120, 25, 100, 50);
+
+                settingJ.add(setTXT);
+                settingJ.add(setBTN);
+                settingJ.add(setPan);
+
+                settingJ.setSize(250, 150);
+                settingJ.setVisible(true);
+
+                settingJ.addWindowListener(new WindowAdapter() {
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        seting.setVisible(false);
+                    }
+                });
+                setBTN.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        try {
+                            String[] settingsStr = setTXT.getText().split(",");
+                            settingNum = Integer.parseInt(settingsStr[0]);
+                            settingSign = Integer.parseInt(settingsStr[1]);
+//                            System.out.println(settingsStr[0] + " " + settingsStr[1]);
+                        } catch (NumberFormatException e1) {
+                            setTXT.setText("only numbers");
+                        }
+                    }
+                });
+            }
+        });
+
         answerText.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
+//                System.out.println(settingNum);
                 super.keyPressed(e);
-                if(e.getKeyCode() == 10) {
+                if (e.getKeyCode() == 10) {
                     try {
-                        questionLabel.setText(checkANS(question, questionLabel, answerText, br, bw));
+                        questionLabel.setText(checkANS(question, questionLabel, answerText, br, bw, settingNum, seting));
                     } catch (IOException ex) {
                         ex.printStackTrace();
                     }
@@ -136,7 +206,7 @@ public class Main {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    questionLabel.setText(checkANS(question, questionLabel, answerText, br, bw));
+                    questionLabel.setText(checkANS(question, questionLabel, answerText, br, bw, settingNum, seting));
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
@@ -152,11 +222,16 @@ public class Main {
         signNum = rndNUmbers[2];
     }
 
-    public static String saveScore(BufferedWriter bw, BufferedReader br, JLabel label, JTextField textField, int score) {
+    public static String saveScore(BufferedWriter bw, BufferedReader br, JLabel label, JTextField textField,
+                                   int score, int settingNum, BufferedWriter bwSet, BufferedReader brSet, int settingSign) {
         String print = "";
         try {
             bw.newLine();
             bw.write(String.valueOf(score));
+            bwSet.newLine();
+            bwSet.write(String.valueOf(settingNum));
+            bwSet.newLine();
+            bwSet.write(String.valueOf(settingSign));
             label.setText("saved");
             print = "saved";
         } catch (IOException e) {
@@ -164,7 +239,10 @@ public class Main {
         } finally {
             try {
                 bw.flush();
+                bwSet.flush();
                 bw.close();
+                bw.close();
+                br.close();
                 br.close();
             } catch (IOException e) {
                 label.setText("problem with saving file: " + e.getMessage());
@@ -172,7 +250,9 @@ public class Main {
         }
         return print;
     }
-    int changeup =0;
+
+    int changeup = 0;
+
     public String newQ(JLabel label) {
         Random numRND = new Random();
 
@@ -184,36 +264,35 @@ public class Main {
         sign[2] = "*";
         sign[3] = "/";
         if (numbers1 < numbers2) { // 1 =1 2 =2
-            changeup =numbers1; // c = 1
+            changeup = numbers1; // c = 1
             numbers1 = numbers2; // 1 =  2
             numbers2 = changeup; // 2 = c(1)
 
         }
         if (signNum > 3) {
-            signNum = numRND.nextInt(4);
+            signNum = numRND.nextInt(settingSign);
         }
-        label.setText(" " +  numbers1 + " " + sign[signNum] + " " + numbers2 + " " );
-        return " " +  numbers1 + " " + sign[signNum] + " " + numbers2 + " ";
+        label.setText(" " + numbers1 + " " + sign[signNum] + " " + numbers2 + " ");
+        return " " + numbers1 + " " + sign[signNum] + " " + numbers2 + " ";
 
     }
 
     public String checkANS(String label, JLabel jLabel, JTextField textField,
-                           BufferedReader br, BufferedWriter bw) throws IOException {
+                           BufferedReader br, BufferedWriter bw, int settingNum, JButton setting) throws IOException {
 
         int ansAI;
         if (textField.getText().toLowerCase().equals("next")) {
             label = newQ(jLabel);
-        } else if (textField.getText().toLowerCase().equals("save")) {
-            label = saveScore(bw, br, jLabel, textField, score);
         } else if (textField.getText().toLowerCase().equals("score")) {
             label = " Your score is: " + score;
-
+        } else if (textField.getText().toLowerCase().equals("settings")) {
+            setting.setVisible(true);
         } else {
             if (signNum == 0) {
                 ansAI = numbers1 + numbers2;
             } else if (signNum == 1) {
                 ansAI = numbers1 - numbers2;
-            }else if (signNum == 2) {
+            } else if (signNum == 2) {
                 ansAI = numbers1 * numbers2;
             } else {
                 ansAI = numbers1 / numbers2;
@@ -225,7 +304,7 @@ public class Main {
                 } else {
                     label += " No, it's: " + ansAI;
                 }
-            }catch (Exception e){
+            } catch (Exception e) {
                 //do nothing, it'll just re-ask a question.
             }
             label += newQ(jLabel);
@@ -235,12 +314,12 @@ public class Main {
 
     public int[] rndNum() {
         Random numRND = new Random();
-        int numbers2 = numRND.nextInt(100);
-        int numbers1 = numRND.nextInt(100);
-        int signNum = numRND.nextInt(4);
+        int numbers2 = numRND.nextInt(settingNum);
+        int numbers1 = numRND.nextInt(settingNum);
+        int signNum = numRND.nextInt(settingSign);
         int[] ints = new int[3];
         if (numbers1 <= numbers2) {
-            numbers1 = numRND.nextInt(100);
+            numbers1 = numRND.nextInt(settingNum);
         }
         ints[0] = numbers1;
         ints[1] = numbers2;
